3004ea55f28f0ffa36fefa734af0cf71444222b3
dlut414 <hfy8373@gmail.com>
2016-10-26 15:52:17 +0900

    Add source

---
+++ Bmp2Geo/Bmp2Geo.cpp
@@ -0,0 +1,129 @@
+// Bmp2Geo.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <bitset>
+using namespace std;
+
+unsigned char* ReadBMP(char* filename, int& width, int& height) {
+	FILE* f;
+	fopen_s(&f, filename, "rb");
+	unsigned char info[54];
+	fread(info, sizeof(unsigned char), 54, f); // read the 54-byte header
+
+	// extract image height and width from header
+	width = *(int*)&info[18];
+	height = *(int*)&info[22];
+
+	int size = 3 * width * height;
+	unsigned char* data = new unsigned char[size]; // allocate 3 bytes per pixel
+	fread(data, sizeof(unsigned char), size, f); // read the rest of the data at once
+	fclose(f);
+
+	for (int i = 0; i < size; i += 3) {
+		unsigned char tmp = data[i];
+		data[i] = data[i + 2];
+		data[i + 2] = tmp;
+	}
+
+	return data;
+}
+
+unsigned char* ReadBMP_padded(char* filename, int& width, int& height) {
+	FILE* f;
+	fopen_s(&f, filename, "rb");
+	if (f == NULL) throw "Argument Exception";
+	unsigned char info[54];
+	fread(info, sizeof(unsigned char), 54, f); // read the 54-byte header
+
+	// extract image height and width from header
+	width = *(int*)&info[18];
+	height = *(int*)&info[22];
+
+	int row_padded = (width * 3 + 3) & (~3);
+	unsigned char* data_row = new unsigned char[row_padded];
+	unsigned char* data = new unsigned char[3 * width* height];
+
+	for (int i = 0; i < height; i++) {
+		fread(data_row, sizeof(unsigned char), row_padded, f);
+		for (int j = 0; j < width; j ++) {
+			// Convert (B, G, R) to (R, G, B)
+			unsigned char b = data_row[3*j];
+			unsigned char g = data_row[3*j + 1];
+			unsigned char r = data_row[3*j + 2];
+			const int id = 3*(width*i + j);
+			data[id] = r;
+			data[id + 1] = g;
+			data[id + 2] = b;
+		}
+	}
+	delete[] data_row;
+	fclose(f);
+	return data;
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int width = 0, height = 0;
+	int NP = 0;
+	double DP;
+	
+	cout << " input initial particle spacing: " << endl;
+	cin >> DP;
+	const unsigned char* const image = ReadBMP_padded("Geo.bmp", width, height);
+	cout << " width: " << width << ", height: " << height << endl;
+
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++) {
+			const int id = 3 * (i*width + j);
+			const unsigned char r = image[id];
+			const unsigned char g = image[id + 1];
+			const unsigned char b = image[id + 2];
+			if (~r || ~g || ~b) NP++;
+		}
+	}
+
+	ofstream file("Geo.in", ofstream::out);
+	file << scientific << setprecision(6) << 0 << endl;
+	file << scientific << setprecision(6) << DP << endl;
+	file << NP << endl;
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++) {
+			const int id = 3 * (i*width + j);
+			const unsigned char r = image[id];
+			const unsigned char g = image[id+1];
+			const unsigned char b = image[id+2];
+			if (r && g && b) continue;
+			const double x = DP* j;
+			const double y = DP* i;
+			///fluid
+			if (~r && ~g && b) {
+				file << 0 << std::scientific << std::setprecision(6) << " " << x << " " << y << " " << 0.0 << " " << 0.0 << " " << 0.0 << endl;
+			}
+			///wall
+			else if (r && ~g && ~b) {
+				file << 1 << std::scientific << std::setprecision(6) << " " << x << " " << y << " " << 0.0 << " " << 0.0 << " " << 0.0 << endl;
+			}
+			///dummy
+			else if (~r && ~g && ~b) {
+				file << 2 << std::scientific << std::setprecision(6) << " " << x << " " << y << " " << 0.0 << " " << 0.0 << " " << 0.0 << endl;
+			}
+			///inlet
+			else if (r && g && ~b) {
+				file << 3 << std::scientific << std::setprecision(6) << " " << x << " " << y << " " << 0.0 << " " << 0.0 << " " << 0.0 << endl;
+			}
+			///outlet
+			else if (~r && g && b) {
+				file << 4 << std::scientific << std::setprecision(6) << " " << x << " " << y << " " << 0.0 << " " << 0.0 << " " << 0.0 << endl;
+			}
+		}
+	}
+	file.close();
+	std::cout << " Writing Geo. done. " << std::endl;
+	
+	return 0;
+}
+
---
+++ Bmp2Geo/ReadMe.txt
@@ -0,0 +1,40 @@
+========================================================================
+    CONSOLE APPLICATION : Bmp2Geo Project Overview
+========================================================================
+
+AppWizard has created this Bmp2Geo application for you.
+
+This file contains a summary of what you will find in each of the files that
+make up your Bmp2Geo application.
+
+
+Bmp2Geo.vcxproj
+    This is the main project file for VC++ projects generated using an Application Wizard.
+    It contains information about the version of Visual C++ that generated the file, and
+    information about the platforms, configurations, and project features selected with the
+    Application Wizard.
+
+Bmp2Geo.vcxproj.filters
+    This is the filters file for VC++ projects generated using an Application Wizard. 
+    It contains information about the association between the files in your project 
+    and the filters. This association is used in the IDE to show grouping of files with
+    similar extensions under a specific node (for e.g. ".cpp" files are associated with the
+    "Source Files" filter).
+
+Bmp2Geo.cpp
+    This is the main application source file.
+
+/////////////////////////////////////////////////////////////////////////////
+Other standard files:
+
+StdAfx.h, StdAfx.cpp
+    These files are used to build a precompiled header (PCH) file
+    named Bmp2Geo.pch and a precompiled types file named StdAfx.obj.
+
+/////////////////////////////////////////////////////////////////////////////
+Other notes:
+
+AppWizard uses "TODO:" comments to indicate parts of the source code you
+should add to or customize.
+
+/////////////////////////////////////////////////////////////////////////////
